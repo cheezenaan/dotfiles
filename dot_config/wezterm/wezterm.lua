d7f35be40b735ef5ba01f842e5271eacf7265869
cheezenaan <cheezenaan@gmail.com>
2025-05-16 18:23:33 +0900

    feat(wezterm): enable auto-reload, IME, and background blur

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+-- 設定ファイルを自動でリロードする
+config.automatically_reload_config = true
+
 -- カラースキームの設定
 config.color_scheme = 'Nord (Gogh)'
 
@@ -12,6 +15,7 @@ config.font = wezterm.font_with_fallback({
   'monospace',
 }, {weight='Regular'})
 config.font_size = 12.0
+config.use_ime = true
 
 config.window_padding = {
   left = 10,
@@ -30,5 +34,6 @@ config.enable_scroll_bar = false
 -- ウィンドウの背景透明度
 config.window_background_opacity = 0.9
 config.text_background_opacity = 0.5
+config.macos_window_background_blur = 20
 
 return config 
