7b8d9269326fa56089e3ef983daee5e95aa73566
cheezenaan <cheezenaan@gmail.com>
2025-07-10 09:52:08 +0900

    feat(zellij): Add terminal multiplexer for enhanced workflow

--- dot_config/zsh/modules/fzf-git.zsh
+++ dot_config/zsh/modules/fzf-git.zsh
@@ -1,5 +1,6 @@
 # ghq + fzf integration
-# CTRL+G でghqで管理しているリポジトリ一覧を表示し、選択したリポジトリにジャンプする
+
+# Ctrl + ] でghqで管理しているリポジトリ一覧を表示し、選択したリポジトリにジャンプする
 function ghq-fzf-cd() {
   local selected_dir=$(ghq list | fzf --query="$LBUFFER")
   if [ -n "$selected_dir" ]; then
@@ -10,7 +11,4 @@ function ghq-fzf-cd() {
 }
 
 zle -N ghq-fzf-cd
-
-# Use CTRL+G for ghq repository selection
-# fzf-git.sh provides CTRL+G+F for git file selection automatically
-bindkey '^g' ghq-fzf-cd
+bindkey '^]' ghq-fzf-cd
